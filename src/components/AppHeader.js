a4fff9d80e83e4bbb3fbf156547c86df1bc6488a
taniaenavarro <tnavarro@icstars.org>
2025-07-16 11:18:48 -0500

    Update AppHeader.js
    Removed icons and profile picture but kept the light and dark mode from the headers

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -6,23 +6,21 @@ import {
   CDropdown,
   CDropdownItem,
   CDropdownMenu,
-  CDropdownToggle,
+  CDropdownToggle,//dark and light mode toggle
   CHeader,
   CHeaderNav,
   CHeaderToggler,
   CNavLink,
-  CNavItem,
   useColorModes,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import {
-  cilBell,
-  cilContrast,
+  cilContrast, //dark and light mode icons
   cilEnvelopeOpen,
   cilList,
   cilMenu,
-  cilMoon,
-  cilSun,
+  cilMoon,//dark and light mode icons
+  cilSun,//dark and light mode icons
 } from '@coreui/icons'
 
 import { AppBreadcrumb } from './index'
@@ -43,6 +41,7 @@ const AppHeader = () => {
   }, [])
 
   return (
+    
     <CHeader position="sticky" className="mb-4 p-0" ref={headerRef}>
       <CContainer className="border-bottom px-4" fluid>
         <CHeaderToggler
@@ -52,34 +51,6 @@ const AppHeader = () => {
           <CIcon icon={cilMenu} size="lg" />
         </CHeaderToggler>
         <CHeaderNav className="d-none d-md-flex">
-          <CNavItem>
-            <CNavLink to="/dashboard" as={NavLink}>
-              Dashboard
-            </CNavLink>
-          </CNavItem>
-          <CNavItem>
-            <CNavLink href="#">Users</CNavLink>
-          </CNavItem>
-          <CNavItem>
-            <CNavLink href="#">Settings</CNavLink>
-          </CNavItem>
-        </CHeaderNav>
-        <CHeaderNav className="ms-auto">
-          <CNavItem>
-            <CNavLink href="#">
-              <CIcon icon={cilBell} size="lg" />
-            </CNavLink>
-          </CNavItem>
-          <CNavItem>
-            <CNavLink href="#">
-              <CIcon icon={cilList} size="lg" />
-            </CNavLink>
-          </CNavItem>
-          <CNavItem>
-            <CNavLink href="#">
-              <CIcon icon={cilEnvelopeOpen} size="lg" />
-            </CNavLink>
-          </CNavItem>
         </CHeaderNav>
         <CHeaderNav>
           <li className="nav-item py-1">
@@ -128,7 +99,6 @@ const AppHeader = () => {
           <li className="nav-item py-1">
             <div className="vr h-100 mx-2 text-body text-opacity-75"></div>
           </li>
-          <AppHeaderDropdown />
         </CHeaderNav>
       </CContainer>
       <CContainer className="px-4" fluid>
@@ -139,3 +109,5 @@ const AppHeader = () => {
 }
 
 export default AppHeader
+
+//AppHeaderDropDown component is the profile picture and dropdown menu but removed
