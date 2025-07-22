753c85abbb9245ecb14425ce56c0347e97a1f4ae
vdewberry76 <vdewberry@icstars.org>
2025-07-22 17:36:37 -0500

    added code to Intergrations tab
    added code

---
+++ src/components/SettingsContent/Intergrations.js
@@ -0,0 +1,18 @@
+import React from 'react';
+ 
+const Integrations = () => {
+  return (
+    <div>
+      <h2>Integrations</h2>
+      <p>Manage and connect third-party tools.</p>
+      <ul>
+        <li>PowerPoint - <button>Connect</button></li>
+        <li>Jira - <button>Connect</button></li>
+        <li>Google Calendar - <button>Connect</button></li>
+      </ul>
+    </div>
+  );
+};
+ 
+export default Integrations;
+ 
